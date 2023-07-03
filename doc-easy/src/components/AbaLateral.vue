e6e59dee84e5b4c92d88ef42a613b9f7ed834941
david.rusycki <davidrusyck@gmail.com>
2023-07-02 22:42:16 -0300

    fix: change draw area to permanent

--- doc-easy/src/components/AbaLateral.vue
+++ doc-easy/src/components/AbaLateral.vue
@@ -1,32 +1,30 @@
 <template>
+  <v-navigation-drawer permanent expand-on-hover rail>
+    <v-list>
+      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="David Rusycki"
+        subtitle="davidrusyck@gmail.com"></v-list-item>
+    </v-list>
 
-      <v-navigation-drawer expand-on-hover rail>
-        <v-list>
-          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="David Rusycki"
-            subtitle="davidrusyck@gmail.com"></v-list-item>
-        </v-list>
-  
-        <v-divider></v-divider>
-  
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-        </v-list>
+    <v-divider></v-divider>
 
-        
-        <template v-slot:append>
-            <div class="pa-2" >
-              <v-btn block>
-                Logout
-              </v-btn>
-            </div>
-          </template>
-      </v-navigation-drawer>
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-folder" title="Plano de documentos" value="planodedocumentos"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-multiple" title="Logs do Sistema" value="logsdosistema"></v-list-item>
+      <v-list-item prepend-icon="mdi-star" title="Random" value="random"></v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn block>
+          Logout
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+
+</template>
   
-  </template>
-  
-  <script setup>
+<script setup>
     //
-  </script>
+</script>
   
