3b06ff99fe9d48d499c9ecb0c85d19ef01124b4c
david.rusycki <davidrusyck@gmail.com>
2023-07-02 22:42:42 -0300

    feat: add cards into content

--- doc-easy/src/views/Base.vue
+++ doc-easy/src/views/Base.vue
@@ -4,7 +4,32 @@
         <AbaLateral></AbaLateral>
 
         <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-            Main Content
+
+            <v-col v-for="n in 3" :key="n" cols="12" sm="4">
+                <v-row no-gutters v-for="n in 3" :key="n">
+
+                    <v-card class="mx-auto mt-4" max-width="344">
+                        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+
+                        <v-card-title>
+                            Top western road trips
+                        </v-card-title>
+
+                        <v-card-subtitle>
+                            1,000 miles of wonder
+                        </v-card-subtitle>
+
+                        <v-card-actions>
+                            <v-btn color="orange-lighten-2" variant="text">
+                                Explore
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-row>
+
+            </v-col>
+
+
         </v-main>
 
     </v-container>
