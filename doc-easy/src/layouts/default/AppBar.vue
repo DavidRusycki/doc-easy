085c9e96924921ba80d26c28df8662e4c666bd6c
david.rusycki <davidrusyck@gmail.com>
2023-07-02 21:45:58 -0300

    feat: icon in app bar

--- doc-easy/src/layouts/default/AppBar.vue
+++ doc-easy/src/layouts/default/AppBar.vue
@@ -1,6 +1,10 @@
 <template>
     <v-app-bar title="Doc Easy" flat>
       
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon icon="mdi-folder"></v-app-bar-nav-icon>
+      </template>
+
     </v-app-bar>
 </template>
 
