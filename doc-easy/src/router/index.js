49e7192b7d787ac0fe647c7dcee6ca31caf8f0c5
david.rusycki <davidrusyck@gmail.com>
2023-07-05 22:18:13 -0300

    feat: add table for admin

--- doc-easy/src/router/index.js
+++ doc-easy/src/router/index.js
@@ -7,26 +7,26 @@ const routes = [
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
-  },
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '/base',
+        path: '/',
         name: 'Base',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Base.vue'),
+      },
+    ],
+  },
+  {
+    path: '/admin/uniedu',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '/admin/uniedu',
+        name: 'Admin',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Admin.vue'),
       },
     ],
   },
---
+++ doc-easy/src/views/Admin.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-container class="fill-height">
+
+        <AbaLateral></AbaLateral>
+
+        <v-main class="d-flex align-center justify-center">
+
+                <v-table fixed-header height="300px">
+                    <thead>
+                        <tr>
+                            <th class="text-left">
+                                Código
+                            </th>
+                            <th class="text-left">
+                                Nome
+                            </th>
+                            <th class="text-left">
+                                Descrição
+                            </th>
+                            <th class="text-left">
+                                Situação
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in desserts" :key="item.name">
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.calories }}</td>
+                        </tr>
+                    </tbody>
+                </v-table>
+
+        </v-main>
+
+
+    </v-container>
+</template>
+  
+<script setup>
+import AbaLateral from '@/components/AbaLateral.vue'
+
+</script>
+
+<style>
+.card-tittle {
+    font-size: 3em !important;
+}
+</style>
+  
