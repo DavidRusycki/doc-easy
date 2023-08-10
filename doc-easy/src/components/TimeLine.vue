7c0d40853f0303388a27a253f5b447235db397a2
david.rusycki <davidrusyck@gmail.com>
2023-08-09 22:33:16 -0300

    feat: time line component
    + init of execution view

---
+++ doc-easy/src/components/TimeLine.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+
+    <v-timeline align="start">
+      <v-timeline-item v-for="(item, i) in items" :key="i" :dot-color="getColor(i)" icon="mdi-file-outline" fill-dot
+        @click="onClickDocument(i)" class="clicavel">
+      </v-timeline-item>
+    </v-timeline>
+
+    <div class="contador">{{ selecao+1 }} de {{ items.length }}</div>
+
+  </div>
+</template>
+
+<style scoped>
+.contador {
+  text-align: center;
+}
+.clicavel {
+
+  cursor: pointer;
+
+}
+</style>
+
+<script>
+export default {
+
+  data: () => ({
+    selecao: 0,
+    items: [
+      {
+        color: 'red-lighten-2',
+        icon: 'mdi-star',
+      },
+      {
+        color: 'purple-lighten-2',
+        icon: 'mdi-book-variant',
+      },
+      {
+        color: 'green-lighten-1',
+        icon: 'mdi-airballoon',
+      },
+      {
+        color: 'indigo-lighten-2',
+        icon: 'mdi-layers-triple',
+      },
+      {
+        color: 'indigo-lighten-2',
+        icon: 'mdi-layers-triple',
+      },
+    ],
+  }),
+  methods: {
+    onClickDocument(indice) {
+      this.updateSelecao(indice);
+    },
+    updateSelecao(indice) {
+      this.selecao = indice;
+    },
+    isSelected(indice) {
+      return indice == this.selecao;
+    },
+    getColor(indice) {
+      return this.isSelected(indice) ? 'green' : 'orange';
+    }
+  }
+}
+</script>
--- doc-easy/src/views/DevComponent.vue
+++ doc-easy/src/views/DevComponent.vue
@@ -1,24 +1,40 @@
 <template>
-  <IndicadorAcao :msg="mensagem" :titulo="titulo"></IndicadorAcao>
+  <div class="tudo">
+    <div class="conteudo">
+      <TimeLine></TimeLine>
+    </div>
+    <div class="area-card">
+      <v-card class="central" title="Card title" text="LOREN IPM SO LUN" width="900" height="800" >
+        <DragDrop multiple="true"></DragDrop>
+      </v-card>
+    </div>
+  </div>
 </template>
 
-<script >
+<style>
+.tudo {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+.central { 
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 0px 3px 0px #00000080 !important;
+}
+</style>
 
+<script>
+import TimeLine from '@/components/TimeLine.vue';
 import DragDrop from '@/components/DragDrop.vue';
-import MensagemModalSucesso from '@/components/MensagemModalSucesso.vue';
-import IndicadorAcao from '@/components/IndicadorAcao.vue';
 
 export default {
   components: {
-    'MensagemModalSucesso': MensagemModalSucesso,
-    'IndicadorAcao': IndicadorAcao,
-  },
-  data() {
-    return {
-      mensagem: 'Registro inserido com sucesso!',
-      titulo: 'Sucesso'
-    }
+    TimeLine: TimeLine,
+    DragDrop: DragDrop
   }
 }
-
 </script>
+
