442087e209ed556c2cc5e984bb723cf216af98ec
david.rusycki <davidrusyck@gmail.com>
2023-10-26 10:50:28 -0300

    fix: remove logout button

--- doc-easy/src/components/AbaLateral.vue
+++ doc-easy/src/components/AbaLateral.vue
@@ -17,11 +17,11 @@
     </v-list>
 
     <template v-slot:append>
-      <div class="pa-2">
+      <!-- <div class="pa-2">
         <v-btn block>
           Logout
         </v-btn>
-      </div>
+      </div> -->
     </template>
   </v-navigation-drawer>
 </template>
